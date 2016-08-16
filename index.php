7199de0c1601a4743f998ab81191e61671dfbbe3
BrianEdward <brian613ass@gmail.com>
2016-08-16 17:27:54 -0400

    Fist commit: h1 tag addded

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include("includes/header.php"); 
+?>
+          <button type="button" class="btn btn-default navbar-btn active" href="#" role="button" id="nav_change">Birmingham Workshop</button>
+          <!--<a class="btn btn-default navbar-btn" href="london.php" role="button">London Workshop</a>-->
+<?php
+include("includes/jumbotron.php"); 
+?>
+        <div class="preheadline">22<span style="text-transform:lowercase;">nd</span> - 24<span  style="text-transform:lowercase;">th</span> September 2016</div>
+        <h1>Mastermind Accelerator <br>3-Day Birmingham Workshop</h1>
+        <p><a class="btn btn-primary btn-lg" href="https://property.infusionsoft.com/app/orderForms/MMA-Birmingham" role="button">Book Your Place</a></p>
+<?php
+include("includes/workshop-summary.php"); 
+?>
+      <h5 class="text-center">3-Day Birmingham Workshop</h5>
+      <ul>
+        <li>Thur 22nd - Sat 24th September 2016</li>
+        <li>Birmingham Crowne Plaza Hotel, NEC</li>
+<?php
+include("includes/order-box.php"); 
+?>
+      <a class="btn btn-primary btn-lg center m-t-md" href="https://property.infusionsoft.com/app/orderForms/MMA-Birmingham" role="button">Book Your Place</a>
+<?php
+include("includes/simon-letter.php"); 
+?>
+          <p>Come and join me in Birmingham on the 22nd - 24th September and step up your game. I promise you will not regret it. And on the off-chance you do, the first day and a half is on me. If at any point up to lunchtime on the second day you decide the workshop isn't for you, simply return the course materials and I'll give you back your money.</p>
+<?php
+include("includes/main.php"); 
+?>
+        <h6 class="text-muted text-uppercase">Birmingham Crowne Plaza Hotel</h6>
+        <h3  class="m-t-0">3-Day Birmingham Residential Workshop</h3>
+        <p class="lead m-b-md">22nd - 24th September 2016</p>
+        <a class="btn btn-primary btn-lg" href="https://property.infusionsoft.com/app/orderForms/MMA-Birmingham" role="button">Book Your Place</a>
+<?php
+include("includes/footer.php"); 
+?>
